1bf859fb51f38443518dee881f54e69f871b1384
Steven Allen <steven@stebalien.com>
2019-01-15 17:19:22 +0000

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Stebalien/go-mod-play
+
+require github.com/Stebalien/go-mod-play/child v2.0.0+incompatible
---
+++ parent.go
@@ -0,0 +1,9 @@
+package parent
+
+import (
+	child "github.com/Stebalien/go-mod-play/child"
+)
+
+func main() {
+	_ = child.Thing
+}
